6fe150a3f55f466e73aed81c1a500a97f4626ded
Jeff <jdost@users.noreply.github.com>
2021-02-27 10:25:58 -0800

    [minivan] Finally test the configs
    - fix up the flashing and compilation code
    - added in backlight controls, got working
    - updated some layer settings to reflect the previous setup

---
+++ minivan/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// hardware definitions
+#define BACKLIGHT_PIN B2
+// backlight settings
+#define BACKLIGHT_LEVELS 4
+#define BACKLIGHT_BREATHING
+// RGB clearing
+#undef  RGB_DI_PIN
+#undef  RGBLED_NUM
+#undef  RGBLIGHT_SLEEP
